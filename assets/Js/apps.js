adacbf69187430c38065be8e4ae25942b01c1707
Alejandra <alejandramellab.gmail.com>
2021-11-17 19:51:23 -0300

    color change funcion added to header h1

--- assets/Js/apps.js
+++ assets/Js/apps.js
@@ -1,15 +1,10 @@
 $(function() {
 
-    $("[data-toggle='tooltip']").tooltip(); //shows directions and extra information.
+    $("[data-toggle='tooltip']").tooltip(); //shows extra information.
 
 
-    $(".card-title").click(function() { // Refresh the appearance of the card content with a click
-        $(".card-text").fadeToggle();
-    });
-
-    $(".somos").on("click", function() { //change the background color of the section with a click on the title
-        $("#us").css("background-color", "#fbce37");
+    $(".titulo").on("click", function() { //change the  color of the title
+        $(this).css("color", "red");
     }, );
-
 
 });
