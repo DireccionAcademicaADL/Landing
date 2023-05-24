226e8be28836db92d23aba7950a78f03eeffa7db
Walter Alarcon Garces <walarcon@gmail.com>
2023-05-23 20:26:23 -0400

    Se incorporó modo oscuro

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$('#Dark_Mode').click(function(){
+    $('.button_di-buffala').css('background-color', '#152768');
+    $('.navbar_di-buffala').css('background-color', '#152768');
+    $('.features__inner-icon').css('background-color', '#152768');
+    $('.footer_di-buffala').css('background-color', '#152768');
+    $('body').css('background-color', '#494949');    
+})
